577bda39388b15bd1affa48d05f5d9dea2b725c1
edy477 <edy47@mesoamericana.edu.gt>
2023-10-07 06:58:38 -0600

    adding table comp

---
+++ src/Stats/PageList.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import {TableContainer} from "@mui/material";
+import TableBody from '@mui/material/TableBody';
+import {Box} from "@mui/material";
+function PageList ()  {
+
+
+
+
+    // @ts-ignore
+    return (
+        <Box>
+
+<p>Test </p>
+
+
+</Box>
+
+    )
+
+
+
+
+}
+export default  PageList;
